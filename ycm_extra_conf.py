ae5c2640126e600530a060621916f2b21b71c629
Wings <wingsdh@gmail.com>
2021-08-15 22:28:30 +0800

    add

---
+++ ycm_extra_conf.py
@@ -0,0 +1,14 @@
+import sys
+def Settings( **kwargs ):
+  return {
+    'sys_path': ['/Users/handeng/opt/anaconda3/bin',
+ '/Users/handeng/opt/anaconda3/lib/python37.zip',
+ '/Users/handeng/opt/anaconda3/lib/python3.7',
+ '/Users/handeng/opt/anaconda3/lib/python3.7/lib-dynload',
+ '',
+ '/Users/handeng/.local/lib/python3.7/site-packages',
+ '/Users/handeng/opt/anaconda3/lib/python3.7/site-packages',
+ '/Users/handeng/opt/anaconda3/lib/python3.7/site-packages/aeosa',
+ '/Users/handeng/opt/anaconda3/lib/python3.7/site-packages/IPython/extensions',
+ '/Users/handeng/.ipython']
+  }
